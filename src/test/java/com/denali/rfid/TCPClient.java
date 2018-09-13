7f9d47a886348582282c68592057b79adff9f989
Ariv <arivbits@gmail.com>
2018-09-13 07:30:52 +0530

    With Stable testing

--- src/test/java/com/denali/rfid/TCPClient.java
+++ src/test/java/com/denali/rfid/TCPClient.java
@@ -17,12 +17,12 @@ public class TCPClient {
 
 	public static void main(String[] args) throws InterruptedException {
 		try {
-			List<String> dateToBePublished = new ArrayList<String>();
-			dateToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 30 39 30 30 32 37 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
-			dateToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 30 37 30 30 31 36 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
-			dateToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 32 34 30 30 34 33 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
-			dateToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 32 32 30 30 31 36 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
-			dateToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 30 38 30 30 31 36 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
+			List<String> dataToBePublished = new ArrayList<String>();
+			dataToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 30 39 30 30 32 37 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
+			dataToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 30 37 30 30 31 36 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
+			dataToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 32 34 30 30 34 33 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
+			dataToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 32 32 30 30 31 36 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
+			dataToBePublished.add("A0 13 01 8A 0C 30 00 54 30 30 38 30 38 30 30 31 36 D8 DB 30 8D A0 05 01 8A 01 22 AD A0 05 01 8A 02 22 AC A0 05 01 8A 03 22 AB A0 05 01 8A 01 22 ");
 			
 			@SuppressWarnings("resource")
 			Socket socket = new Socket("192.168.43.70", 4001);
@@ -30,7 +30,7 @@ public class TCPClient {
 			PrintWriter writer = new PrintWriter(output, true);
 			while (true) {
 				int value = new Random().nextInt(5);
-				writer.println(dateToBePublished.get(value));
+				writer.println(dataToBePublished.get(value));
 			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
